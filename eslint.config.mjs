2a0134d1364f41b10fda51a7a6cc30e0f2375647
utsobe <mmhutsobe01@gmail.com>
2025-06-24 23:01:49 +0800

    basic project setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      // eqeqeq: "off",
+      'no-unused-vars': 'error',
+      'no-unused-expressions': 'error',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+      'no-console': 'warn',
+      'no-undef': 'error',
+    },
+  },
+  {
+    ignores: ['.node_modules/*'],
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
